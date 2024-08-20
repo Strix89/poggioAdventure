7fb2d0c72adcdc882d245f0440cce396076a69fa
Elia Valenza <retro26@Retro-Pc>
2024-08-20 18:13:59 +0200

    Aggiunta commenti parser

--- poggioAdventure/src/main/java/di/uniba/map/b/adventure/parser/Parser.java
+++ poggioAdventure/src/main/java/di/uniba/map/b/adventure/parser/Parser.java
@@ -19,14 +19,15 @@ public class Parser {
 
     private final Set<String> stopwords;
 
-    /**
-     *
-     * @param stopwords
-     */
-    public Parser(Set<String> stopwords) {
+    // Costruttore, setta l'insieme di stringhe che non devono essere considerate
+    public Parser(Set<String> stopwords) { 
         this.stopwords = stopwords;
     }
 
+    /*
+    Controlla se il comando dato in input è presente nella lista di comandi o è associato ad un alias, se si ritorna la posizione del 
+    comando nella lista, altrimenti -1
+    */ 
     private int checkForCommand(String token, List<Command> commands) {
         for (int i = 0; i < commands.size(); i++) {
             if (commands.get(i).getName().equals(token) || commands.get(i).getAlias().contains(token)) {
@@ -36,6 +37,10 @@ public class Parser {
         return -1;
     }
 
+    /*
+    Controlla se l'oggetto dato in input è presente nella lista di oggetti o è associato ad un alias, 
+    se si ritorna la posizione dell'oggetto nella lista, altrimenti -1
+    */
     private int checkForObject(String token, List<AdvObject> obejcts) {
         for (int i = 0; i < obejcts.size(); i++) {
             if (obejcts.get(i).getName().equals(token) || obejcts.get(i).getAlias().contains(token)) {
@@ -49,50 +54,40 @@ public class Parser {
     * frasi semplici del tipo <azione> <oggetto> <oggetto>. Eventuali articoli o preposizioni vengono semplicemente
     * rimossi.
      */
-
-    /**
-     *
-     * @param command
-     * @param commands
-     * @param objects
-     * @param inventory
-     * @return
-     */
-
     public ParserOutput parse(String command, List<Command> commands, List<AdvObject> objects, List<AdvObject> inventory) {
-        List<String> tokens = Utils.parseString(command, stopwords);
-        if (!tokens.isEmpty()) {
-            int ic = checkForCommand(tokens.get(0), commands);
-            if (ic > -1) {
-                if (tokens.size() > 1) {
-                    int io = checkForObject(tokens.get(1), objects);
-                    int ioinv = -1;
-                    if (io < 0 && tokens.size() > 2) {
-                        io = checkForObject(tokens.get(2), objects);
+        List<String> tokens = Utils.parseString(command, stopwords); // Rimuove le parole non significative
+        if (!tokens.isEmpty()) { 
+            int ic = checkForCommand(tokens.get(0), commands); // Controlla se il primo token è un comando
+            if (ic > -1) { // Se il comando è presente
+                if (tokens.size() > 1) { // Se ci sono altri token
+                    int io = checkForObject(tokens.get(1), objects); // Controlla se il secondo token è un oggetto se si restituisce la posizione dell'oggetto nella lista degli oggetti
+                    int ioinv = -1;  // Inizializza la posizione dell'oggetto nell'inventario a -1
+                    if (io < 0 && tokens.size() > 2) { // se l'oggetto non è presente e ci sono più di due token
+                        io = checkForObject(tokens.get(2), objects); // Controlla se il terzo token è un oggetto
                     }
-                    if (io < 0) {
-                        ioinv = checkForObject(tokens.get(1), inventory);
-                        if (ioinv < 0 && tokens.size() > 2) {
-                            ioinv = checkForObject(tokens.get(2), inventory);
+                    if (io < 0) { // Se l'oggetto non è presente
+                        ioinv = checkForObject(tokens.get(1), inventory); // Controlla se il secondo token è un oggetto dell'inventario
+                        if (ioinv < 0 && tokens.size() > 2) { // Se l'oggetto dell'inventario non è presente e ci sono più di due token
+                            ioinv = checkForObject(tokens.get(2), inventory); // Controlla se il terzo token è un oggetto dell'inventario
                         }
                     }
-                    if (io > -1 && ioinv > -1) {
-                        return new ParserOutput(commands.get(ic), objects.get(io), inventory.get(ioinv));
-                    } else if (io > -1) {
-                        return new ParserOutput(commands.get(ic), objects.get(io), null);
-                    } else if (ioinv > -1) {
-                        return new ParserOutput(commands.get(ic), null, inventory.get(ioinv));
+                    if (io > -1 && ioinv > -1) { // Se l'oggetto esiste ed è presente nell'inventario
+                        return new ParserOutput(commands.get(ic), objects.get(io), inventory.get(ioinv)); // Restituisce il comando, l'oggetto e l'oggetto dell'inventario
+                    } else if (io > -1) { // Se l'oggetto esiste
+                        return new ParserOutput(commands.get(ic), objects.get(io), null); // Restituisce il comando e l'oggetto
+                    } else if (ioinv > -1) { // Se l'oggetto dell'inventario esiste
+                        return new ParserOutput(commands.get(ic), null, inventory.get(ioinv)); // Restituisce il comando e l'oggetto dell'inventario
                     } else {
-                        return new ParserOutput(commands.get(ic), null, null);
+                        return new ParserOutput(commands.get(ic), null, null); // Restituisce solo il comando
                     }
                 } else {
                     return new ParserOutput(commands.get(ic), null);
                 }
             } else {
-                return new ParserOutput(null, null);
+                return new ParserOutput(null, null); // Se il comando non è presente
             }
         } else {
-            return null;
+            return null; // Se non ci sono token
         }
     }
 
--- poggioAdventure/src/main/java/di/uniba/map/b/adventure/parser/ParserOutput.java
+++ poggioAdventure/src/main/java/di/uniba/map/b/adventure/parser/ParserOutput.java
@@ -18,74 +18,45 @@ public class ParserOutput {
 
     private AdvObject object;
     
-    private AdvObject invObject;
+    private AdvObject invObject; // oggetto di tipo AdvObject che rappresenta l'inventario
 
-    /**
-     *
-     * @param command
-     * @param object
-     */
+    /*
+     * Costruttore che inizializza il ParserOutput con il comando e l'oggetto
+    */
     public ParserOutput(Command command, AdvObject object) {
         this.command = command;
         this.object = object;
     }
 
-    /**
-     *
-     * @param command
-     * @param object
-     * @param invObejct
-     */
+    /*
+     * Costruttore che inizializza il ParserOutput con il comando, l'oggetto e l'oggetto dell'inventario
+    */
     public ParserOutput(Command command, AdvObject object, AdvObject invObejct) {
         this.command = command;
         this.object = object;
         this.invObject = invObejct;
     }
 
-    /**
-     *
-     * @return
-     */
     public Command getCommand() {
         return command;
     }
 
-    /**
-     *
-     * @param command
-     */
     public void setCommand(Command command) {
         this.command = command;
     }
 
-    /**
-     *
-     * @return
-     */
     public AdvObject getObject() {
         return object;
     }
 
-    /**
-     *
-     * @param object
-     */
     public void setObject(AdvObject object) {
         this.object = object;
     }
 
-    /**
-     *
-     * @return
-     */
     public AdvObject getInvObject() {
         return invObject;
     }
 
-    /**
-     *
-     * @param invObject
-     */
     public void setInvObject(AdvObject invObject) {
         this.invObject = invObject;
     }
