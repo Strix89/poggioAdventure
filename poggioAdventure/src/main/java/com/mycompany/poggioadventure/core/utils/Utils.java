3329fda34e340fdd9e2f22b03f277d67bee5196c
Strix89 <orlandotommaso48@gmail.com>
2025-06-18 22:25:28 +0200

    Fixed levels time

--- poggioAdventure/src/main/java/com/mycompany/poggioadventure/core/utils/Utils.java
+++ poggioAdventure/src/main/java/com/mycompany/poggioadventure/core/utils/Utils.java
@@ -70,8 +70,8 @@ public class Utils {
 
     // ==================== CONFIGURAZIONE LIVELLI ====================
     
-    /** Limite tempo Livello 1: --> 10 min*/ 
-    public static final long LEVEL_1_TIME_LIMIT = 10 * 60 * 1000;
+    /** Limite tempo Livello 1: --> 20 min*/ 
+    public static final long LEVEL_1_TIME_LIMIT = 20 * 60 * 1000;
     
     /** ID completamento Livello 1 */
     public static final int OBJ_LEVEL1_COMPLETE_ID = 100;
@@ -83,7 +83,7 @@ public class Utils {
     public static final List<Integer> LEVEL_1_FORBIDDEN_OBJECTS = List.of(OBJ_LOSE_GAME_ID);
 
     /** Limite tempo Livello 2: --> 15 min*/
-    public static final long LEVEL_2_TIME_LIMIT = 15 * 60 * 1000;
+    public static final long LEVEL_2_TIME_LIMIT = 25 * 60 * 1000;
     
     /** ID completamento Livello 2 */
     public static final int OBJ_LEVEL2_COMPLETE_ID = 200;
@@ -95,7 +95,7 @@ public class Utils {
     public static final List<Integer> LEVEL_2_FORBIDDEN_OBJECTS = List.of(OBJ_LOSE_GAME_ID);
 
     /** Limite tempo Livello 3: 15 minuti */
-    public static final long LEVEL_3_TIME_LIMIT = 15 * 60 * 1000;
+    public static final long LEVEL_3_TIME_LIMIT = 25 * 60 * 1000;
     
     /** ID completamento Livello 3 */
     public static final int OBJ_LEVEL3_COMPLETE_ID = 300;
